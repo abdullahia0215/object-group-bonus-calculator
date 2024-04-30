16e39b19d22cf00812f5e92ebe226d84780db1b9
Gabrielle Lommel <granolagabrielle@gmail.com>
2024-04-30 13:33:00 -0500

    Complete function

--- client.js
+++ client.js
@@ -43,15 +43,51 @@ console.log('array of employee data: ',  employees );
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-
-
-
 // This function will calculate 1 employee's bonus!
 //
 function calculateIndividualEmployeeBonus( employee ) {  
   // your logic here
+  let totalBonus;
+  for (let person of employee) {
+    // nested loop to search for rating
+    for (let reviewRating of person) {
+      if (reviewRating <= 2) {
+        totalBonus + 0
+      } else if (reviewRating === 3) {
+        totalBonus + (0.04 * person.annualSalary);
+      } else if (reviewRating === 4) {
+        totalBonus + (0.06 * person.annualSalary);
+      } else if (reviewRating === 5) {
+        totalBonus + (0.1 * person.annualSalary);
+      } 
+      }
+    for (let salary of person) {
+      if (person.annualSalary > 65000) {
+        totalBonus - (0.01 * person.annualSalary)
+      }
+    }
+    for (let i=0; i < employee[i].employeeNumber.length; i++){
+      if (employeeNumber.length === 4){
+        totalBonus + (0.05 * employee[i].annualSalary);
+      }
+    }
+  }
+  }
   
+
+  
+  
+  
+// ----------BONUS LOGIC--------------
+  // rating of a 2 or below = 0% bonus
+  // rating of 3  = 4% bonus
+  // rating of 4 = 6% bonus
+  // rating of 5 = 10% bonus
+  // if employee number is 4 digits long, give an additional 5%
+  // if annual income is greater than 65k, adjust bonus down 1%
+// -----------------------------------
+
   
   // return new object with bonus results
-
+return totalBonus;
 }
